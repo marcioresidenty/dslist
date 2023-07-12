4300b52575cc4bcb35196f0351cd634278cd426e
marcioresidenty <marcioresidenty@hotmail.com>
2023-07-11 21:12:10 -0300

    Domain model

---
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,65 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity //ela configura a classe java pra ser equivalente a tabela relacional
+@Table(name = "tb_belonging") //mapeia a tabela no banco de dados 
+public class Belonging {
+
+	//classe auxiliar para representar uma chave primaria
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	
+	private Integer position;
+	
+	public Belonging() {
+		
+	}
+
+	public Belonging(Game game, GameList list, Integer position) {
+	
+		id.setGame(game);
+		id.setList(list);
+		this.position = position;
+	}
+
+	public BelongingPK getId() {
+		return id;
+	}
+
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
