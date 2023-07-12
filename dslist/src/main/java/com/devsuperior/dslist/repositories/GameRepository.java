3b2f81f53fb5c7d2ef886fb5b26599bc2006a8c7
marcioresidenty <marcioresidenty@hotmail.com>
2023-07-11 22:04:32 -0300

    GET game by id

--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslist.entities.Game;
 
-// e um coponente do sistema 
+// e um coponente do sistema que tem os componentes basicos 
 public interface GameRepository extends JpaRepository<Game, Long>{
 
 }
--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -16,7 +18,16 @@ public class GameService {
 	@Autowired //injeta uma instacia do GameRepository, componente do sistema 
 	private GameRepository gameRepository;
 	
+	//Busca por id
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		}
+	
+	
 	//Retorna um DTO
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
