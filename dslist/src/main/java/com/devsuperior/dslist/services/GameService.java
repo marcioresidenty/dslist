0bce2239928666b46798058ff5690a7f45be7b12
marcioresidenty <marcioresidenty@hotmail.com>
2023-07-11 23:14:01 -0300

    Consulta Sql, projection busca de games por lista

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 
@@ -34,4 +35,18 @@ public class GameService {
 		return dto;
 		
 	}
+	//Retorna um DTO
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	
+		
+	}
 }
+
+
+
+
+
+
